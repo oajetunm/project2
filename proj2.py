613d297c639c339e97bc1dd560c7e99657edc6a0
mwnewman <mwnewman@umich.edu>
2017-02-07 23:21:50 -0500

    added proj2.py

---
+++ proj2.py
@@ -0,0 +1,29 @@
+#proj2.py
+
+
+#### Problem 1 ####
+print('\n*********** PROBLEM 1 ***********')
+print('New York Times -- First 10 Story Headings\n')
+
+### Your Problem 1 solution goes here
+
+
+#### Problem 2 ####
+print('\n*********** PROBLEM 2 ***********')
+print('Michigan Daily -- MOST READ\n')
+
+### Your Problem 2 solution goes here
+
+
+#### Problem 3 ####
+print('\n*********** PROBLEM 3 ***********')
+print("Mark's page -- Alt tags\n")
+
+### Your Problem 3 solution goes here
+
+
+#### Problem 4 ####
+print('\n*********** PROBLEM 4 ***********')
+print("UMSI faculty directory emails\n")
+
+### Your Problem 4 solution goes here
